ea22ad65e98bcf12bdfa02b02492de9bbcebd50c
ilaunchingltd-prog <ilaunching.ltd@gmail.com>
2025-12-05 14:38:42 +0000

    Fix foreign_keys: use lambda to defer column reference evaluation

--- models/__init__.py
+++ models/__init__.py
@@ -22,10 +22,19 @@ from models.user_navigation import UserNavigation
 from sqlalchemy.orm import relationship
 
 # Add navigation relationship to UserProfile
-UserProfile.navigation = relationship("UserNavigation", back_populates="user_profile", uselist=False, foreign_keys="[UserNavigation.user_profile_id]")
+UserProfile.navigation = relationship(
+    "UserNavigation", 
+    back_populates="user_profile", 
+    uselist=False, 
+    foreign_keys=lambda: [UserNavigation.user_profile_id]
+)
 
 # Update UserNavigation relationship to include back_populates
-UserNavigation.user_profile = relationship("UserProfile", back_populates="navigation", foreign_keys="[UserNavigation.user_profile_id]")
+UserNavigation.user_profile = relationship(
+    "UserProfile", 
+    back_populates="navigation", 
+    foreign_keys=lambda: [UserNavigation.user_profile_id]
+)
 
 __all__ = [
     'UserProfile',
